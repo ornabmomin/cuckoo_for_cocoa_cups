e40d47ff62f7fcf7c7ebbd41da28bd93b9270cc9
ornab <ornab@ornab.ca>
2023-01-03 00:09:28 +0000

    new

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Cuckoo for cocoa puffs</title>
         </Head>
         <Container>
           <Intro />
